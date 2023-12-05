5b4c3c681cfa77ef9093193b909063b78944b54f
Alice <alycecil@gmail.com>
2023-12-04 22:16:37 -0500

    update for steam library

--- src/main/java/SetupMods.java
+++ src/main/java/SetupMods.java
@@ -4,9 +4,11 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
 public class SetupMods {
+    public static final String steam_location = "D:\\SteamLibrary";//"C:\\Program Files (x86)\\Steam";
+
     public static void main(String... args) throws IOException {
-        File modsStream = new File("C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\211820");
-        File modsFolderInstall = new File("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Starbound\\mods");
+        File modsStream = new File(steam_location + "\\steamapps\\workshop\\content\\211820");
+        File modsFolderInstall = new File(steam_location + "\\steamapps\\common\\Starbound\\mods");
         for (File file : modsStream.listFiles()) {
             if(!file.isDirectory()) continue;
 
